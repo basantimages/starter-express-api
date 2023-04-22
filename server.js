f12c1ed8ac0fee77634f9f933e25cbcf8b7e0871
basantimages <124242939+basantimages@users.noreply.github.com>
2023-04-22 17:33:55 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const cors = require('cors');
+const express = require('express');
+const app = express();
+
+// Add middleware to parse JSON request bodies
+app.use(express.json());
+app.use(cors());
+
+app.post('/brs/data', async (req, res) => {
+  try {
+    const response = await fetch(`https://www.instagram.com/${req.body.input}/?__a=1&__d=dis`); // Replace with your actual API endpoint
+    const resp = await response.json();
+    console.log(resp);
+    res.json(resp);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch data' });
+  }
+});
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
