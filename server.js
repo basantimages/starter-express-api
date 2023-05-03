a36cf78b45accd84a098faaadb09c8fdb001d580
basantimages <124242939+basantimages@users.noreply.github.com>
2023-05-03 12:38:46 +0530

    Node mailer server file
    Deleted old server.js file and adding new server.js file for contact form mailing using gmail.

--- server.js
+++ server.js
@@ -1,86 +1,50 @@
-import cors from 'cors';
 import express from 'express';
-import axios from 'axios';
+import cors from 'cors';
+import nodemailer from 'nodemailer';
+const router = express.Router();
+
+// server used to send send emails
 const app = express();
-
-// Section 1 =====================================================================
-const headers = {
-  'User-Agent':
-    'Instagram 105.0.0.18.119 (iPhone11,8; iOS 14_3; en_US; en-US; scale=2.00; 828x1792; 219842854)',
-  'x-ig-app-id': '1217981644879628',
-};
-
-// Add middleware to parse JSON request bodies
+app.use(cors());
 app.use(express.json());
-app.use(
-  cors({
-    origin: 'http://localhost:5173',
-  }),
-);
+app.use('/', router);
+app.listen(5000, () => console.log('Server Running'));
 
-// Set CORS headers for all API endpoints
-app.use((_, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Referrer-Policy', 'no-referrer');
-  next();
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'basantrajbusiness@gmail.com',
+    pass: 'pwsqjmnpakcomijp',
+  },
 });
 
-app.post('/brs/data', async (req, res) => {
-  try {
-    const url = `https://i.instagram.com/api/v1/users/web_profile_info/?username=${req.body.input}`;
-
-    axios
-      .get(url, { headers })
-      .then((response) => {
-        // console.log(response.data);
-        res.json(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch data' });
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Ready to Send');
   }
 });
 
-app.get('/brs/data', async (req, res) => {
-  try {
-    res.json({ error: 'Success' });
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch data' });
-  }
+router.post('/contact', (req, res) => {
+  const name = req.body.fName + ' ' + req.body.lName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mail = {
+    from: name,
+    to: 'basantrajbusiness@gmail.com',
+    subject: `Message from ${name}`,
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: 'Message Sent' });
+    }
+  });
 });
-
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
-});
-
-// Section 2 =====================================================================
-// Adding middleware to parse JSON request bodies
-// app.use(express.json());
-// app.use(cors());
-
-// app.post('/brs/data', async (req, res) => {
-//   try {
-//     const response = await fetch(`https://www.instagram.com/${req.body.input}/?__a=1&__d=dis`); // Replace with your actual API endpoint
-//     const resp = await response.json();
-//     console.log(resp);
-//     res.json(resp);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Failed to fetch data' });
-//   }
-// });
-
-// app.get('/brs/data', async (req, res) => {
-//   try {
-//     res.json({ success: 'Success' });
-//   } catch (error) {
-//     res.status(500).json({ error: 'Failed to fetch data' });
-//   }
-// });
-
-// app.listen(3000, () => {
-//   console.log('Server is running on port 3000');
-// });
